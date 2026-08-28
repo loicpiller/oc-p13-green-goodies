6e3a002e32f4628d1168cec4c725ea52af835042
Loïc PILLER <loicpiller@mail.fr>
2026-08-28 14:45:30 +0200

    feat(fixtures): add a user in AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setFirstName('Jean')
+            ->setLastName('Dupont')
+            ->setEmail('user@test.com')
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
